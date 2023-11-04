13d151a9e0e106b050e9af910a0a122969719f91
Aaron1360 <aaronmvilleda@gmail.com>
2023-11-03 18:20:46 -0600

    requirements added

--- web_scraper.py
+++ web_scraper.py
@@ -1,2 +1,12 @@
 """This script shows how to retrieve the content of a web page using requests and Beautiful Soup.
 The page we are going to work on is a sandbox of www.scrapethissite.com"""
+
+import requests
+from bs4 import BeautifulSoup as bs
+import openpyxl as xl
+
+page = "https://www.scrapethissite.com/pages/simple/"
+
+response = requests.get(page)
+with open("content.txt","w") as content:
+    content.write(str(response.content))
