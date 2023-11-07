bf7a290a4eefe29c2d04511a7ead2820594f7872
Aaron1360 <aaronmvilleda@gmail.com>
2023-11-07 16:22:46 -0600

    comments added

--- hockey_teams.py
+++ hockey_teams.py
@@ -12,17 +12,19 @@ response = requests.get(page)
 response.encoding = "latin-1"
 soup = bs(response.content,"html.parser",from_encoding="latin-1")
 
+# Create dataframe
 table = soup.find("table")
-# print(table.prettify())
 columns = table.find_all("th")
 column_titles = [col.text.strip() for col in columns]
 df = pd.DataFrame(columns = column_titles)
-# print(column_titles)
+
+# Fill dataframe 
 column_data = table.find_all("tr")
 for row in column_data[1:]:
     row_data = row.find_all("td")
     individual_row_data = [data.text.strip() for data in row_data]
-    # print(individual_row_data)
     length = len(df)
     df.loc[length] = individual_row_data
+
+# Create excel file
 df.to_excel("dataframe.xlsx",index=False)
