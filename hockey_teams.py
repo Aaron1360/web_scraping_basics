65481f9ed24df6af6917e5ad66bed41dc36b388e
Aaron1360 <aaronmvilleda@gmail.com>
2023-11-07 21:17:20 -0600

    scrape all tables

--- hockey_teams.py
+++ hockey_teams.py
@@ -3,28 +3,57 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import time
 
-# Target
-page = "https://www.scrapethissite.com/pages/forms/?per_page=100"# here you can change the number of rows per page (25/50/100)
+# URLs
+PAGE = "https://www.scrapethissite.com/pages/forms/?per_page=100"# here you can change the number of rows per page (25/50/100)
+MAIN_WEBSITE = "https://www.scrapethissite.com"
 
-# Make request and parse content
-response = requests.get(page)
-response.encoding = "latin-1"
-soup = bs(response.content,"html.parser",from_encoding="latin-1")
+def new_request(page):
+    response = requests.get(page)
+    response.encoding="latin-1"
+    soup = bs(response.content,"html.parser",from_encoding="latin-1")
+    return soup
+
+def scrape_table(current_table,dataframe):
+    rows = current_table.find_all("tr")
+    for row in rows[1:]:
+        row_cells = row.find_all("td")
+        row_data = [data.text.strip() for data in row_cells]
+        length = len(dataframe)
+        dataframe.loc[length] = row_data
+
+def scrape_page(link,dataframe):
+    soup = new_request(link)
+    current_table = soup.find("table",class_="table")
+    scrape_table(current_table,dataframe)
+    
+# Make first request 
+soup = new_request(PAGE)
+
+# Get list of links to the next pages
+pagination_area = soup.find("ul",class_="pagination")
+pages = pagination_area.find_all("a")
+links = [MAIN_WEBSITE + link.get("href") for link in pages[1:-1]]
 
 # Create dataframe
-table = soup.find("table")
-columns = table.find_all("th")
-column_titles = [col.text.strip() for col in columns]
-df = pd.DataFrame(columns = column_titles)
+first_table = soup.find("table",class_="table")
+column_titles = first_table.find_all("th")
+titles = [title.text.strip() for title in column_titles]
+hockey_df = pd.DataFrame(columns=titles)
 
-# Fill dataframe 
-column_data = table.find_all("tr")
-for row in column_data[1:]:
-    row_data = row.find_all("td")
-    individual_row_data = [data.text.strip() for data in row_data]
-    length = len(df)
-    df.loc[length] = individual_row_data
+# Fill dataframe
+scrape_table(first_table,hockey_df)
+for link in links:
+    time.sleep(1)
+    scrape_page(link,hockey_df)
 
-# Create excel file
-df.to_excel("dataframe.xlsx",index=False)
+# Save data on a excel file
+hockey_df.to_excel("dataframe.xlsx",index=False)
+
+"""
+1- make first request and get all the pages excluding the first one and the forward button **DONE**
+2- scrape data of the first page and append it to the dataframe **DONE**
+3- make new request with a delay of one second to get the next page and scrape recursively 
+4- append data to dataframe on each request
+5- generate excel file"""
